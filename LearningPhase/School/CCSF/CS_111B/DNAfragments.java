e13f9f9c422921e4d93da0dd4fbdd0b0d2f2d6a3
Ziky Zhang <lee4our@gmail.com>
2024-03-07 22:43:53 -0800

    git fin CS_111B/DNAfragments.java

--- LearningPhase/School/CCSF/CS_111B/DNAfragments.java
+++ LearningPhase/School/CCSF/CS_111B/DNAfragments.java
@@ -1,8 +1,50 @@
+// Author: Xirong (Ziky) Zhang & Shawn Luna & Azeal Riday
+// Date of Last Modification: 03/06/2024
+// Course: CS-111B
+// Instructor: C. Conner
+// Assignment #4-2
+// File Name: DNAfragments.java
+//
+/* Algorithms:
+ *     1. Declare variables
+ *     2. Get arrayLength & arrayContent
+ *     3. Initialize variables
+ *   ───────────────────────────────────────────────────────────────
+ *     1. print values in reverse
+ *     ┌─────────┐
+ *     |         ↓
+ *     |  a. count the index number down
+ *     |  b. print the according value in the array
+ *     └─────────┘
+ *
+ *     2. print the longest value
+ *     ┌─────────┐
+ *     |         ↓
+ *     |  a. read current value's length
+ *     |     i. count characters
+ *     |  b. compare with the longest value yet
+ *     |     if current value is longer:
+ *     |     i. replace
+ *     └─────────┘
+ *        c. print results.
+ *
+ *     3. ask the user to delete value
+ *        a. get input from user
+ *        b. check if input matches with any existing values
+ *           if matches:
+ *           i. create new variables
+ *           ii. put the original value in except for the new input value.
+ *           iii. print result.
+ *           if not:
+ *           i. print error.
+ */
+
 import java.util.Scanner;
 
 public class DNAfragments {
     public static void main(String args[]) {
         Scanner scnr = new Scanner(System.in);
+
         int i;
 
         // user input initialize values
@@ -66,5 +108,6 @@ public class DNAfragments {
         } else {
             System.out.println(removedFrag + " is not on the list.");
         }
+
     }
 }
