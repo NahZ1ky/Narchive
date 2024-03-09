bdf681f63f6ae364807c7d0901476c689264b8a2
Ziky Zhang <lee4our@gmail.com>
2024-03-08 17:12:37 -0800

    git in CS_111B/TicTacToeStub.java git fin CS_111B/Fragment_zver.java

--- LearningPhase/School/CCSF/CS_111B/Fragment_zver.java
+++ LearningPhase/School/CCSF/CS_111B/Fragment_zver.java
@@ -1,7 +1,7 @@
 // Author: Ziky
 // Assignment: DNAfragments.java
 // Part: #4
-// Last Modification: 03/07/2024
+// Last Modification: 03/08/2024
 // Compatibility to Original: false
 
 import java.util.*;
@@ -12,10 +12,10 @@ public class Fragment_zver {
 
         // initialize fragmentList
         System.out.println("How many fragments do you want in this program?");
-        int numFragment;
-        numFragment = scanner.nextInt();
-        String[] fragmentList = new String[numFragment];
-        for (int i = 0; i < numFragment; i++){
+        int fragmentCount;
+        fragmentCount = scanner.nextInt();
+        String[] fragmentList = new String[fragmentCount];
+        for (int i = 0; i < fragmentCount; i++){
             System.out.print("Enter DNA fragment: ");
             fragmentList[i] = scanner.next();
         }
@@ -24,34 +24,42 @@ public class Fragment_zver {
         String targetFragmentName;
         System.out.println("\nWhat fragment would you like to delete? ");
         targetFragmentName = scanner.next();
-        int matchedFragmentNum = 0;
-        for (int i = 0; i < numFragment; i++){
+        int matchedFragmentCount = 0;
+        for (int i = 0; i < fragmentCount; i++){
             if (targetFragmentName.equals(fragmentList[i])){
-                matchedFragmentNum++;
+                matchedFragmentCount++;
             }
         }
-        int newNumFragment = numFragment - matchedFragmentNum;
+        int newFragmentCount = fragmentCount - matchedFragmentCount;
 
         // print results
-        if (newNumFragment == numFragment){
+        if (newFragmentCount == fragmentCount){
             System.out.println("There are no matched fragment");
-        } else if (newNumFragment == 0){
+            return;
+        } else if (newFragmentCount == 0){
             System.out.println("There is no fragment left after deletion");
+            return;
         } else {
-            String[] newFragmentList = new String[newNumFragment];
+            /*
+            String[] newFragmentList = new String[newFragmentCount];
             int newFragmentIndex = 0;
-            for (int i = 0; i < newNumFragment; i++){
-                if (targetFragmentName != fragmentList[i]){
+            for (int i = 0; i < fragmentCount; i++){
+                if (!targetFragmentName.equals(fragmentList[i])){
                     newFragmentList[newFragmentIndex] = fragmentList[i];
                     newFragmentIndex++;
                 }
+            } */
+            ArrayList<String> newFragmentList = new ArrayList<String>();
+            for (int i = 0; i < fragmentCount; i++){
+                if (!targetFragmentName.equals(fragmentList[i])){
+                    newFragmentList.add(fragmentList[i]);
+                }
             }
-            System.out.println("The fragment list after the deletion of " + matchedFragmentNum + " " + targetFragmentName + " is shown below.");
+            System.out.println("The fragment list after the deletion of " + matchedFragmentCount + " " + targetFragmentName + " is shown below.");
             System.out.println("Updated fragment list: " );
-            for (int i = 0; i < newNumFragment; i++){
-                System.out.print(newFragmentList[i] + " ");
+            for (int i = 0; i < newFragmentCount; i++){
+                System.out.print(newFragmentList.get(i) + " ");
             }
         }
-
     }
 }
