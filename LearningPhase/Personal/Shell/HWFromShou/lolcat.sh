5f4c8ce67ccb431cc89ce75914c6b84f3a8bc854
Ziky Zhang <lee4our@gmail.com>
2024-03-12 15:15:39 -0700

    Shell/HWFromShou/lolcat.sh  add user input and system output

--- LearningPhase/Personal/Shell/HWFromShou/lolcat.sh
+++ LearningPhase/Personal/Shell/HWFromShou/lolcat.sh
@@ -1,1 +1,10 @@
-lolcat
+#! /bin/zsh
+
+# Goal: colorize its input text with random colors.
+echo "How many times do you want this program to run?"
+read runCount
+
+for i in "runCount"; do
+    echo "Type something and I'll echo it"
+    read lolInput
+    
