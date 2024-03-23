e33f33ea20c5b1602a27009030cb5f7169d7b68f
Ziky Zhang <lee4our@gmail.com>
2024-03-22 17:15:58 -0700

    Create math chapter

---
+++ Python/math/operator.py
@@ -0,0 +1,21 @@
+# math.operator
+
+## addition
+meowTime = 0
+addMeowTime = meowTime + 2            # -> 0 + 2
+addMeowTime += 1                      # -> 2 + 1
+
+for i in range (addMeowTime):
+    print("meow")
+
+num = 5 - 2
+for i in range (num):
+    print("meow")
+
+num = 1 * 3
+for i in range (num):
+    print("meow")
+
+num = 12 / 4
+for i in range(num):
+    print("meow")
---
+++ Python/math/variables.py
@@ -0,0 +1,18 @@
+# math.variable
+
+## variables
+food = -10
+furCount = 894502458
+bodyTemp = 38.7
+
+## specify variables
+age = int(3)                  # -> 3
+weight = float(5.8)           # -> 5.8
+kitties = str(3)              # -> '3'
+
+## casting
+strAge = str(age)             # -> '3'
+intWeight = int(weight)       # -> 2
+intKitties = int(kitties)     # -> 3
+strBodyTemp = str(bodyTemp)   # -> '38.7'
+floatFood = float(food)       # -> -10.0
