e1011f2e138b197fc40d34f45b257a6176860dcd
Ziky Zhang <lee4our@gmail.com>
2024-04-06 17:23:19 -0700

    Add null checkers

--- projects/CS50xPuzzleDay2024/roomDecor.java
+++ projects/CS50xPuzzleDay2024/roomDecor.java
@@ -191,27 +191,23 @@ public class roomDecor {
     }
 
     // In the upstairs of the house, the wall color must match the color of at least one of the objects in the room. In the downstairs, the wall color must not match the color of any of the objects in the room.
-    /**
-     * @param layout
-     * @return
-     */
     public static boolean condition10(String[][] layout){
         int countBathroom = 0;
         int countBedroom = 0;
         int countLivingRoom = 0;
         int countKitchen = 0;
         for (int i = 1; i < 5; i++){
-            if (layout[1][i] != null && layout[1][1] != null && layout[1][i].equals(layout[1][1])){
-            countBathroom++;
+            if (layout[1][i] != null && layout[1][i].equals(layout[1][1])){
+                countBathroom++;
             }
-            if (layout[2][i] != null && layout[2][1] != null && layout[2][i].equals(layout[2][1])){
-            countBedroom++;
+            if (layout[2][i] != null && layout[2][i].equals(layout[2][1])){
+                countBedroom++;
             }
-            if (layout[3][i] != null && layout[3][1] != null && layout[3][i].equals(layout[3][1])){
-            countLivingRoom++;
+            if (layout[3][i] != null && layout[3][i].equals(layout[3][1])){
+                countLivingRoom++;
             }
-            if (layout[4][i] != null && layout[4][1] != null && layout[4][i].equals(layout[4][1])){
-            countKitchen++;
+            if (layout[4][i] != null && layout[4][i].equals(layout[4][1])){
+                countKitchen++;
             }
         }
         if (countBathroom == 0 || countBedroom == 0 ||
@@ -267,7 +263,6 @@ public class roomDecor {
                 count_multiple++;
             }
         }
-
         return count_multiple == 1;
     }
 
@@ -277,29 +272,29 @@ public class roomDecor {
         int countDownstairs = 0;
         for (int i = 0; i < 4; i++){
             if (i < 3){ // upstairs
-                if (layout[i][1].equals("R")){
+                if (layout[i][1] != null && layout[i][1].equals("R")){
                     countUpstairs++;
                 }
-                if (layout[i][2].equals("G")){
+                if (layout[i][2] != null && layout[i][2].equals("G")){
                     countUpstairs++;
                 }
-                if (layout[i][3].equals("B")){
+                if (layout[i][3] != null && layout[i][3].equals("B")){
                     countUpstairs++;
                 }
-                if (layout[i][4].equals("Y")){
+                if (layout[i][4] != null && layout[i][4].equals("Y")){
                     countUpstairs++;
                 }
             } else {    // downstairs
-                if (layout[i][1].equals("R")){
+                if (layout[i][1] != null && layout[i][1].equals("R")){
                     countDownstairs++;
                 }
-                if (layout[i][2].equals("G")){
+                if (layout[i][2] != null && layout[i][2].equals("G")){
                     countDownstairs++;
                 }
-                if (layout[i][3].equals("B")){
+                if (layout[i][3] != null && layout[i][3].equals("B")){
                     countDownstairs++;
                 }
-                if (layout[i][4].equals("Y")){
+                if (layout[i][4] != null && layout[i][4].equals("Y")){
                     countDownstairs++;
                 }
             }
@@ -316,29 +311,29 @@ public class roomDecor {
         int countModern = 0;
         for (int i = 1; i < 5; i++){
             // count unusual objects
-            if (layout[i][1].equals("R")){
+            if (layout[i][1] != null && layout[i][1].equals("R")){
                 countUnusual++;
             }
-            if (layout[i][2].equals("G")){
+            if (layout[i][2] != null && layout[i][2].equals("G")){
                 countUnusual++;
             }
-            if (layout[i][3].equals("B")){
+            if (layout[i][3] != null && layout[i][3].equals("B")){
                 countUnusual++;
             }
-            if (layout[i][4].equals("Y")){
+            if (layout[i][4] != null && layout[i][4].equals("Y")){
                 countUnusual++;
             }
             // count modern objects
-            if (layout[i][1].equals("Y")){
+            if (layout[i][1] != null && layout[i][1].equals("Y")){
                 countModern++;
             }
-            if (layout[i][2].equals("R")){
+            if (layout[i][2] != null && layout[i][2].equals("R")){
                 countModern++;
             }
-            if (layout[i][3].equals("G")){
+            if (layout[i][3] != null && layout[i][3].equals("G")){
                 countModern++;
             }
-            if (layout[i][4].equals("B")){
+            if (layout[i][4] != null && layout[i][4].equals("B")){
                 countModern++;
             }
         }
@@ -422,65 +417,44 @@ public class roomDecor {
                     layout[4][3] = ALL_COLORS_PLUS_EMPTY[digits[6]];
                     layout[4][4] = ALL_COLORS_PLUS_EMPTY[digits[7]];
 
-                    /* if (count++ % 10000 == 0) {
-                        System.out.println(count);
-                        printLayout(layout);
-                    } */
+                    if (count++ % 10000 == 0) {
+                        // System.out.println(count);
+                        // printLayout(layout);
+                    }
 
                     if (testLayout(layout)) {
                         printLayout(layout);
                         return;
                     }
                 }
-                // System.out.println("count at " + wallColors + " is " + count);
+                System.out.println("count at " + wallColors + " is " + count);
             }
         }
     }
 
     public static boolean testLayout(String[][] layout) {
-        if (!condition7(layout)) {
+        if (!condition7(layout))
             return false;
-        }
-
-        if (!condition3(layout)) {
+        if (!condition3(layout))
             return false;
-        }
-
-        if (!condition2(layout)) {
+        if (!condition2(layout))
             return false;
-        }
-
-        if (!condition6(layout)) {
+        if (!condition6(layout))
             return false;
-        }
-
-        if (!condition8(layout)) {
+        if (!condition8(layout))
             return false;
-        }
-
-        if (!condition10(layout)) {
+        if (!condition10(layout))
             return false;
-        }
-
-        if (!condition11(layout)) {
+        if (!condition11(layout))
             return false;
-        }
-
-        if (!condition12(layout)) {
+        if (!condition12(layout))
             return false;
-        }
-
-        if (!condition13(layout)) {
+        if (!condition13(layout))
             return false;
-        }
-
-        if (!condition14(layout)) {
+        if (!condition14(layout))
             return false;
-        }
-
-        if (!condition5(layout)) {
+        if (!condition5(layout))
             return false;
-        }
         return true;
     }
 
