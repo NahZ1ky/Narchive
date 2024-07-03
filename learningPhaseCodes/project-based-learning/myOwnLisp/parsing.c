be138576fd4c200d386c6715aa25ce3c248e7285
Ziky Zhang <lee4our@gmail.com>
2024-07-02 17:18:29 -0700

    Chapter 6 - Polish Notation Grammer

--- learningPhaseCodes/project-based-learning/myOwnLisp/parsing.c
+++ learningPhaseCodes/project-based-learning/myOwnLisp/parsing.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <editline/readline.h>
+
+#ifdef _WIN32 // use following if compiling on windows
+  #include <string.h>
+
+  static char buffer[2048];
+
+  char *readline(char *prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(coy, buffer);
+   cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+  }
+
+  void add_history(char *unused) {}
+
+#else // otherwise include the editline headers
+  #include <editline/readline.h>
+#endif
+
+
 
 int main(int argc, char **argv) {
   // Print Version and Exit Information
@@ -14,13 +35,12 @@ int main(int argc, char **argv) {
 
     printf("No, you are a %s\n", input); // echo user input
     */
-
+    
     char* input = readline("lispy> "); // output prompt and get input in one line
     add_history(input); // add input to history
 
     printf("No you're a %s\n", input); // echo user input
     free(input); // free retrieved input
-    
   }
   return 0;
 }
